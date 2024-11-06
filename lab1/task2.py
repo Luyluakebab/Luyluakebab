8bdd6b58b65c8633bd3f129c15bdc70716f21e6b
Luyluakebab <durbazevajulia@gmail.com>
2024-11-06 17:14:54 +0500

    Add files via upload

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,1 +1,10 @@
+from math import cos, log, exp, sin
 
+x = float(input("Введите значение переменной x: "))
+y = float(input("Введите значение переменной y: "))
+z = float(input("Введите значение переменной z: "))
+
+a = ((x**3)/2)**1/2 - sin(y)
+b = exp(2)/3 - cos(y) + z + log(y)
+
+print(f"Получено значение функции a={a}")
